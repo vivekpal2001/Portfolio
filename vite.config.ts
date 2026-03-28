b1f0aa16dde330c63d9c29b7f37e1df800886c49
Vivek_Kumar/1901 <kumarvivekmailme@gmail.com>
2026-03-28 12:58:32 +0530

    endpoint fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   // Since you are using a custom domain, the base MUST be the root path ('/')
-  base: '/',
+  base: '/Portfolio/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
